609dc828a9c13cc8943fec6a2067487349feb231
BenjaminANgafua <benjaminarkutl2017@gmail.com>
2021-03-23 11:48:22 +0000

    creating capital

--- myapp.js
+++ myapp.js
@@ -30,28 +30,44 @@ let counties = {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/home.html"));
 });
-app.post('submit', function(req, res) {
-    console.log(req.body)
-})
+
 app.get('/counties', (req, res) => {
     res.send(counties);
     // fs.readFile("/county-capital.json", "utf8", (err, data) => {
-    //     let
+
+    //     let json_data = JSON.parse(data)
+    //     res.end(JSON.stringify(json_data))
+    //     if (err) {
+    //         console.error(err.message)
+    //     }
     // })
-    // res.sendFile(path.join(__dirname + "/index.html"));
+    // res.sendFile(path.join(__dirname + "/counties.html"));
 
 });
 
-app.get('/counties/:countyCapital', (req, res) => {
-    const Capital = req.params.countyCapital;
-    res.send(counties[Capital]);
-    console.log(req.body)
+app.get('/counties/capital/:Capital', (req, res) => {
+    const capital = req.params.Capital;
+    res.send(`Capital:${counties[capital]}`);
+    console.log(counties[capital])
+
+    // res.sendFile(path.join(__dirname + "/capital.html"));
+
 });
-app.get('/counties/create/:newcounty', (req, res) => {
-    const county = req.params.newcounty;
-    // counties.push(county);
-    res.send({ county });
-    console.log("See new county", { county })
+app.get('/create/county/:county/names/:capital', (req, res) => {
+    console.log('Data::::::::::::>>>>>>>>>>', req.params)
+    let county = req.params
+    res.send(county);
+    console.log("The newly created county is", county)
+
+    // res.sendFile(path.join(__dirname + "/create-county.html"));
+
+    //     // counties.push(county);
+    // res.send({ county: capital });
+    // console.log("See new county", { county: capital })
+})
+app.post('/create/county', (req, res) => {
+    let new_county = req.body
+    console.log('What is this', new_county)
 })
 
 const port = process.env.PORT || 3900;
